83fd4619a21528af73bf43f9ac7f3e8d41a323d1
vennybear <vennybear233@gmail.com>
2026-03-01 15:30:58 -0800

    Update Display_Device_Test.ino

--- code/Display_Device_Test.ino
+++ code/Display_Device_Test.ino
@@ -1,31 +1,28 @@
 /* 
- * Door Detective - Display Device Test Code
+ * Door Detective - XIAO ESP32C3 Test Code
  * Author: Yuna Xiong
  */
 
 #include <Stepper.h>
 
-// X27.168 Stepper Motor settings
-const int stepsPerRevolution = 600; 
-Stepper myStepper(stepsPerRevolution, 14, 26, 27, 25);
+Stepper myStepper(600, 2, 4, 3, 5);
 
-const int buttonPin = 33;
-const int ledPin = 32;
+const int ledPin = 6; 
+const int buttonPin = 7; 
 
 void setup() {
   Serial.begin(115200);
-  myStepper.setSpeed(30); // X27 motors move better at lower speeds
+  myStepper.setSpeed(30); 
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // Check if the button is pressed
   if (digitalRead(buttonPin) == LOW) {
     digitalWrite(ledPin, HIGH);
-    myStepper.step(100); // Move needle forward
+    myStepper.step(100); 
     delay(1000);
-    myStepper.step(-100); // Move needle back
+    myStepper.step(-100); 
     digitalWrite(ledPin, LOW);
     delay(500);
   }
